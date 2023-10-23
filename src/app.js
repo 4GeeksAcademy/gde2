e14c704d9d7743b8deee9efd41f85e0d8ffe5273
Braiton57 <143307681+Braiton57@users.noreply.github.com>
2023-10-23 06:06:02 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+  let action = ["se comió", "orinó en", "aplastó", "rompió"];
+  let what = ["mi tarea", "las llaves", "el auto"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  function generarExcusa() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excusaGenerada = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+    document.getElementById("excuse").innerHTML = excusaGenerada;
+  }
+
+  // Asignar la función generarExcusa al botón
+  document.querySelector("button").addEventListener("click", generarExcusa);
 };
